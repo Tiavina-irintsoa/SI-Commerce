99e5f42d7e73a5a7b5e3b59a2db331805304af99
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2023-11-16 18:37:58 +0300

    vue validation et besoin par nature

--- sql/views.sql
+++ sql/views.sql
@@ -22,3 +22,35 @@ create or replace view v_article as(
     from article
     where datesuppression is null
 );
+
+create or replace view v_besoin_valide as(
+    select
+    *
+    from besoin 
+    where datevalidation is not null
+);
+create or replace view v_besoin_non_consulte as(
+    select 
+    * 
+    from besoin
+    where dateValidation is null and dateRefus is null
+);
+create or replace view v_besoin_semaine as (
+    select * from v_besoin_valide
+    where extract(week from dateBesoin)  = extract(week from now()) and extract(year from dateBesoin) = extract (year from now())
+);
+create or replace view v_besoin_semaine_avant_non_consulte as(
+    select * from v_besoin_non_consulte
+    where extract(week from dateBesoin)  = extract(week from now()) and extract(year from dateBesoin) = extract (year from now())
+);
+create or replace view v_besoin_par_nature as 
+    select
+    sum(detailBesoin.quantite),v_article.nomArticle,detailBesoin.idArticle
+    from
+    detailBesoin
+    join v_besoin_valide
+        on v_besoin_valide.idBesoin = detailBesoin.idBesoin
+    join v_article
+        on v_article.idArticle = detailBesoin.idArticle
+    group by detailBesoin.idArticle,v_article.nomArticle;
+
