ffb846ef85bff8d692c6cbbac16bd0947e74f31b
Rebeka-1892 <etu1892@gmail.com>
2023-11-17 23:22:13 +0300

    pdf proforma

---
+++ application/controllers/PDF.php
@@ -0,0 +1,26 @@
+&<?php
+
+    defined('BASEPATH') or exit('No direct script access allowed');
+    
+    class PDF extends CI_Controller{
+
+        public function __construct() {
+            parent::__construct();
+            $this->load->model('pdfmodel' , 'pdf'); // Chargez le modèle générique
+        }
+
+        public function index(){
+            ob_start();
+            $data = array(
+                array("Colonne 1", "Colonne 2"),
+                array("Donnée 1", "Donnée 2"),
+                array("Donnée 4", "Donnée 5")
+            );
+            $societe = 'Miraculous LadyBug';
+            $f = 'Fournisseur';
+            $delai = '08-08-2023';
+            $this->pdf->demande_proforma($societe, $f, $delai, $data);            
+            ob_end_flush();       
+        }
+    }
+?>
---
+++ application/models/PdfModel.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PdfModel extends CI_Model{
+    public function demande_proforma($societe, $fournisseur, $delai, $data){
+        require_once(APPPATH . 'libraries/fpdf/fpdf.php');
+
+        $pdf = new FPDF();
+        $pdf->AddPage();
+
+        $pdf->SetFont('Arial','B',24);
+        $pdf->Cell(0, 20, 'DEMANDE DE PROFORMA', 0, 5, 'C');
+
+        $pdf->SetFont('Arial', 'B', 16);
+        $pdf->Cell(30,20,'Societe : ');
+        $pdf->SetFont('Arial','',16);
+        $pdf->Cell(60,20, $societe);
+        $pdf->SetFont('Arial', 'B', 16);
+        $pdf->Cell(40,20,'Fournisseur :');
+        $pdf->SetFont('Arial','',16);
+        $pdf->Cell(60,20, $fournisseur);
+
+        $pdf->Ln();
+        $pdf->SetFont('Arial', 'B', 16);
+        $pdf->Cell(55, 0,'Delai de livraison : ');
+        $pdf->SetFont('Arial','',16);
+        $pdf->Cell(50, 0,$delai);
+
+        $pdf->Ln();
+        $pdf->Cell(10,20,'',);
+        $pdf->Ln();
+        $pdf->SetFont('Arial', 'B', 16);
+        $pdf->Cell(35,15,'',0, 0, 'C');
+        $pdf->Cell(60,15,'Designation',1, 0, 'C');
+        $pdf->Cell(60,15,'Quantite',1, 0 , 'C');
+        $pdf->Ln();
+        $pdf->SetFont('Arial','',16);
+        foreach($data as $row) {
+            $pdf->Cell(35,15,'',0, 0, 'C');
+            $pdf->Cell(60, 15, $row[0], 1, 0, 'C');
+            $pdf->Cell(60, 15, $row[1], 1, 0, 'C');
+            $pdf->Ln();
+        }
+
+        $pdf->Output('proforma.pdf', 'I');
+    }
+}
