68cc9aa3dd29ddc65b8787aacd02522571b02f04
Rebeka-1892 <etu1892@gmail.com>
2023-11-18 22:26:59 +0300

    pdf bdc

--- application/controllers/PDF.php
+++ application/controllers/PDF.php
@@ -22,5 +22,19 @@
             $this->pdf->liste_proforma($societe, $f, $delai, $data);            
             ob_end_flush();       
         }
+
+        public function bon_de_commande(){
+            ob_start();
+            $data = array(
+                array("Colonne 1", "Colonne 2"),
+                array("Donnée 1", "Donnée 2"),
+                array("Donnée 4", "Donnée 5")
+            );
+            $societe = 'Miraculous LadyBug';
+            $f = 'Fournisseur';
+            $delai = '08-08-2023';
+            $this->pdf->content_bdc($societe, $f, $delai, $data);            
+            ob_end_flush();       
+        }
     }
 ?>
